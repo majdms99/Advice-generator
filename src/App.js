09e351fcfcd2b8f1ae59250d212fabf759d7e098
msalami <msalami@solutions-integration.com>
2022-04-07 12:12:56 +0300

    upload files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
-
+import image from './images/pattern-divider-desktop.svg'
+import Icone from './images/icon-dice.svg'
 function App() {
+
+  /*state*/
+  const [advices, setAdvices] = useState('');
+
+  /*Get data from API*/
+  const fetchData = () => {
+    fetch('https://api.adviceslip.com/advice')
+      .then(res => res.json())
+      .then(result => {
+        setAdvices(result.slip);
+      })
+  }
+  // console.log(advices);
+
+  const handelClick = () => {
+    fetchData()
+  }
+
+  /*Run function {fetchData} when page loads*/
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='advice-box'>
+        <h4 className='advice-number'>Advice #{advices.id}</h4>
+        <q className='advice'>{advices.advice}</q>
+        <img className='image' src={image} alt='advice' />
+        <button onClick={handelClick}><img src={Icone} alt='icon' /></button>
+      </div>
+
+      <p className='copyright'>Created by <span>Majd Salameh</span></p>
     </div>
   );
 }
